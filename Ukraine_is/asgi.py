d25915574e895cda94afcc1528c4e1c9f9cface4
Anastasiia <puddinjoker18@gmail.com>
2020-11-08 02:34:01 +0200

    'initial'

---
+++ Ukraine_is/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ukraine_is project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ukraine_is.settings')
+
+application = get_asgi_application()
