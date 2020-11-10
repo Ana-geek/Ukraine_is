3308b630572d4a97401eecd7f9f1455195593567
Anastasiia <puddinjoker18@gmail.com>
2020-11-11 01:40:36 +0200

    'ini'

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import index, gambit
+from .views import index, gambit, multipage, amongus, sign_in, register
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='homepage'),
+    path('/', index, name='homepage'),
     # path('htmltrain', htmltrain),
-    # path ('about', about)
-    path('gambit', gambit)
+    path('gambit', gambit, name='gambit'),
+    path('multi_page/<int:page_id>', multipage, name='multipage'),
+    path('among_us/<int:page_id>', amongus, name='amongus'),
+    path('login', sign_in, name='loginpage'),
+    path('register', register, name='register'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
     return render(request, 'home/index.html')
@@ -15,3 +13,32 @@ def index(request):
 def gambit(request):
     return render(request, 'home/gambit.html')
 
+
+def multipage(request, page_id: int):
+    print(page_id)
+    data = {
+        'page__id': page_id,
+        'text': 'hello to you',
+        'animals': ['cat', 'dog', 'bat', 'owl'],
+        'range': range(10)
+    }
+    return render(request, 'home/multi-value-page.html', context=data)
+
+
+def amongus(request, page_id: int):
+    print(page_id)
+    data = {
+        'page__id': page_id,
+        'text': 'well, well...',
+        'roles': ['crew', 'impostor', 'crew', 'impostor', 'crew', 'crew', 'crew', 'impostor', 'crew', 'crew'],
+        'players': range(1, 11)
+    }
+    return render(request, 'home/among_us.html', context=data)
+
+
+def sign_in(request):
+    return render(request, 'home/login.html')
+
+
+def register(request):
+    return render(request, 'home/register.html')
